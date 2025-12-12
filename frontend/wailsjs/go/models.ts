324f96f1c4fd476e5bc814d4712e389f19f07955
gabe-crafting <gabriel.projects90@proton.me>
2025-12-12 10:14:53 +0200

    add sidebar recent open files

--- frontend/wailsjs/go/models.ts
+++ frontend/wailsjs/go/models.ts
@@ -1,5 +1,19 @@
 export namespace main {
 	
+	export class FileHistoryItem {
+	    filePath: string;
+	    timestamp: number;
+	
+	    static createFrom(source: any = {}) {
+	        return new FileHistoryItem(source);
+	    }
+	
+	    constructor(source: any = {}) {
+	        if ('string' === typeof source) source = JSON.parse(source);
+	        this.filePath = source["filePath"];
+	        this.timestamp = source["timestamp"];
+	    }
+	}
 	export class OpenFileResult {
 	    filePath: string;
 	    content: string;
